898b44455ef20fcb20250da3b342814ee265a864
Dmitriy Nikiforov <d.nikiforov@ion.team>
2019-07-01 17:56:49 +0700

    added toInitials util

--- app/src/main/java/ru/skillbranch/devintensive/Utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/Utils/Utils.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.Utils
 
+import java.io.File
+
 object  Utils {
     fun parseFullName(fullName: String?):Pair<String?, String?>{
         val parts: List<String>? = fullName?.split(" ")
@@ -9,5 +11,12 @@ object  Utils {
         if(firstName?.length == 0) firstName = null
         if(lastName?.length == 0) lastName = null
         return firstName to lastName
+    }
+
+    fun toInitials(firstName: String?, lastName: String?):String?{
+        val f = firstName?.trim()?.getOrNull(0)
+        val l = lastName?.trim()?.getOrNull(0)
+        val result = listOfNotNull(f, l).joinToString("")
+        return if(result == "") null else result.toUpperCase()
     }
 }
