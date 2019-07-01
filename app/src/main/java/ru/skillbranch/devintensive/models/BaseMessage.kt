28ff2fd1f2402a5529cc0916ec9e6d3f456a3112
Dmitriy Nikiforov <d.nikiforov@ion.team>
2019-07-01 15:37:28 +0700

    created abstract factory for *Message

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -2,6 +2,11 @@ package ru.skillbranch.devintensive.models
 
 import java.util.*
 
+enum class MessageTypes{
+    TEXT,
+    IMAGE
+}
+
 abstract class BaseMessage(
     val id: String,
     val from: User?,
@@ -11,4 +16,16 @@ abstract class BaseMessage(
 ) {
 
     abstract fun formatMessage():String
+
+    companion object AbstractFactory{
+        var lastId = -1
+        fun makeMessage(from:User?, chat:Chat, date:Date = Date(), type:MessageTypes = MessageTypes.TEXT, payload:Any?):BaseMessage{
+            lastId++
+            return when(type)
+            {
+                MessageTypes.TEXT -> TextMessage("$lastId", from, chat, date=date, text = payload as String)
+                MessageTypes.IMAGE -> ImageMessage("$lastId", from, chat, date=date, image = payload as String)
+            }
+        }
+    }
 }
