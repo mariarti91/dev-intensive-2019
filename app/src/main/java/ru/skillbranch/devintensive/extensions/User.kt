88cfb591aa4db99fbb6c1403f7107fbbd2d86e24
Dmitriy Nikiforov <d.nikiforov@ion.team>
2019-07-02 18:37:03 +0700

    added some files

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+
+fun User.toUserView(): UserView{
+
+    val nickName = ""
+    val initials = ""
+    val status = if (lastVisit == null) "Ни разу не был" else if (isOnline) "online" else "Последний раз был ${humanizeDiff()}"
+
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        nickName = nickName,
+        initials = initials,
+        avatar = avatar,
+        status = status
+    )
+}
+
+fun humanizeDiff(): String {
+    return ""
+}
