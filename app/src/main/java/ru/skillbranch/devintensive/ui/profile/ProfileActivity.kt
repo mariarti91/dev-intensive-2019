5a64f7a65de2a341c5a93976923a0895e3ebecdf
Dmitriy Nikiforov <d.nikiforov@ion.team>
2019-07-24 12:52:13 +0700

    fixed rotation crush

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -11,13 +11,6 @@ import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.models.Bender
 
 class ProfileActivity : AppCompatActivity() {
-    lateinit var benderImage: ImageView
-
-    lateinit var textTxt: TextView
-
-    lateinit var messageEt: EditText
-    lateinit var sendBtn: ImageView
-    lateinit var benderObj: Bender
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -27,9 +20,6 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        outState?.putString("STATUS", benderObj.status.name)
-        outState?.putString("QUESTION", benderObj.question.name)
-        outState?.putString("USERINPUT", messageEt.text.toString())
-        Log.d("M_MainActivity", "onSaveInstanceStatus ${benderObj.status.name} ${benderObj.question.name}")
+        Log.d("M_MainActivity", "onSaveInstanceStatus")
     }
 }
